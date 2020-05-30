fa76125da7dbac74d5d0dfc13593b1c372caf304
yusuke ohno <yusuke@ohno.systems>
2020-05-31 02:09:08 +0900

    init

---
+++ functions/nuxt-app/pages/unko.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>unko</div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('VXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVXVX')
+  },
+}
+</script>
+
+<style scoped></style>
